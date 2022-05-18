a6b444e263f7fd2631c71d85cfb835943470be6b
Fırat Alçın <firatalcin04@gmail.com>
2022-05-18 17:17:18 +0300

    Repository Interfaces

---
+++ Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T,bool>> filter);
+        IQueryable<T> Where(Expression<Func<T,bool>> filter);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> filter);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}

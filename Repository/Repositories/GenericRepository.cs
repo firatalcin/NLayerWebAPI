e4cc7968210e3b4baf1cd903c8aed25469be8e06
Fırat Alçın <firatalcin04@gmail.com>
2022-05-26 20:43:49 +0300

    ProductController

--- Repository/Repositories/GenericRepository.cs
+++ Repository/Repositories/GenericRepository.cs
@@ -15,7 +15,7 @@ namespace Repository.Repositories
         protected readonly AppDbContext _context;
         private readonly DbSet<T> _dbSet;
 
-        public GenericRepository(AppDbContext context, DbSet<T> dbSet)
+        public GenericRepository(AppDbContext context)
         {
             _context = context;
             _dbSet = _context.Set<T>();
@@ -43,7 +43,7 @@ namespace Repository.Repositories
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _dbSet.FindAsync(id);
+            return await _dbSet.FindAsync(id) ?? throw new InvalidOperationException(); 
         }
 
         public void Remove(T entity)

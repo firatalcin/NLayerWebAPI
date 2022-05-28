f0b813cc3f7ff7b282eb4d9759f80efc93eece49
Fırat Alçın <firatalcin04@gmail.com>
2022-05-28 16:20:39 +0300

    GetCategoryByIdWithProducts Endpoint

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetSignleCategoryByIdWithProducts(int categoryId)
+        {
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+        }
+    }
+}
